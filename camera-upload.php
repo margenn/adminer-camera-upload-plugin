51b6c900c26f44497fbd112be5a18da9b22b394f
Marcelo Gennari <margenn@gmail.com>
2024-10-27 02:01:10 -0300

    Photos are stored in subfolder that follows db structure: DB / TABLE / FIELD. Bugfix: When the field function was set to NULL, image got lost

--- camera-upload.php
+++ camera-upload.php
@@ -1,13 +1,14 @@
 <?php
 
 /** Edit fields ending with "_photo" with a camera upload interface and link to the uploaded photos from select
-* the connection MUST be HTTPS otherwise you get a 'No supported webcam interface found'
-* @link https://www.adminer.org/plugins/#use
-* @author Marcelo Gennari, https://gren.com.br/
-* @license https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
-* @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
-* @version 1.0.3
-*/
+ * This plugin was based on file-upload.php
+ * the connection MUST be HTTPS otherwise you get a 'No supported webcam interface found'
+ * @link https://www.adminer.org/plugins/#use
+ * @author Marcelo Gennari, https://gren.com.br/
+ * @license https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
+ * @version 1.1.0
+ */
 class AdminerCameraUpload {
 	/** @access protected */
 	var $uploadPath, $displayPath, $scripts;
@@ -18,15 +19,15 @@ class AdminerCameraUpload {
 	* @param string required js libraries
 	* @param string fields terminated with this regex, will activate this plugin
 	*/
-	function __construct($uploadPath = "./photos/", $displayPath = null, $scripts = array("webcam.min.js"), $fieldsufix = '_photo') {
-		$this->uploadPath = $uploadPath;
-		$this->displayPath = ($displayPath !== null ? $displayPath : $uploadPath);
+	function __construct($uploadPath = "./photos", $displayPath = null
+			, $scripts = array("webcam.min.js"), $fieldsufix = '_photo') {
+		$this->uploadPath = rtrim($uploadPath, '/');
+		$this->displayPath =  $displayPath ? rtrim($displayPath, '/') : $this->uploadPath;
 		$this->scripts = $scripts;
-		$this->fieldsufix = '~' . $fieldsufix . '$~';
+		$this->fieldsufix = '~(.*)' . $fieldsufix . '$~';
 	}
 
 	function head() {
-		if(!isset($_GET["edit"])) return;
 		foreach ($this->scripts as $script) {
 			echo script_src($script);
 		}
@@ -36,13 +37,13 @@ class AdminerCameraUpload {
 	function editInput($table, $field, $attrs, $value) {
 		if (preg_match($this->fieldsufix, $field["field"])) {
 			$fieldname = "$field[field]";
-			$rtn = "";
-			if (! empty($value)) {
-				$rtn .= "<div><a href=\"$this->displayPath$table/$value\">$value</a></div>";
+			$return = "";
+			if (!empty($value)) { // link to the attached file
+				$return .= "<a href='$this->displayPath/$_GET[db]/$table/$fieldname/$value'> <div id='attachedfile'>$value</div> </a>";
 			}
-			$rtn .= "<input type='hidden' id='$fieldname' name='$fieldname'>\n";
-			$rtn .= "<div id='preview_$fieldname'></div>\n";
-			$rtn .= script("
+			$return .= "<input type='hidden' id='$fieldname' name='$fieldname'>\n";
+			$return .= "<div id='preview_$fieldname'></div>\n";
+			$return .= script("
 			function start_$fieldname() {
 				if (! Webcam.loaded) {
 					var preview_w=200, preview_h=150, resolution_multiplier=3.2;
@@ -76,38 +77,46 @@ class AdminerCameraUpload {
 				document.getElementById('redo_$fieldname').style.visibility = 'hidden';
 				document.getElementById('ok_$fieldname').style.visibility = 'hidden';
 				document.getElementById('start_$fieldname').style.visibility = 'visible';
+				var function_$fieldname = document.getElementsByName('function[$fieldname]')[0];
+				if (function_$fieldname.value == 'NULL') {
+					function_$fieldname.value = ''
+				}
 			}
 			");
-			$rtn .= "<input type=button id='start_$fieldname' value='Start'>" . script("qs('#start_$fieldname').onclick = start_$fieldname;");
-			$rtn .= "<input type=button id='snap_$fieldname' value='Snap' style='visibility:hidden;'>" . script("qs('#snap_$fieldname').onclick = snap_$fieldname;");
-			$rtn .= "<input type=button id='redo_$fieldname' value='Redo' style='visibility:hidden;'>" . script("qs('#redo_$fieldname').onclick = redo_$fieldname;");
-			$rtn .= "<input type=button id='ok_$fieldname' value='OK' style='visibility:hidden;'>" . script("qs('#ok_$fieldname').onclick = ok_$fieldname;");
-			return $rtn;
+			$return .= "<input type=button id='start_$fieldname' value='Start'>" . script("qs('#start_$fieldname').onclick = start_$fieldname;");
+			$return .= "<input type=button id='snap_$fieldname' value='Snap' style='visibility:hidden;'>" . script("qs('#snap_$fieldname').onclick = snap_$fieldname;");
+			$return .= "<input type=button id='redo_$fieldname' value='Redo' style='visibility:hidden;'>" . script("qs('#redo_$fieldname').onclick = redo_$fieldname;");
+			$return .= "<input type=button id='ok_$fieldname' value='OK' style='visibility:hidden;'>" . script("qs('#ok_$fieldname').onclick = ok_$fieldname;");
+			return $return;
 		}
-		// no return means NULL, which trigger this method in other plugins until reach the original overloaded method
+		// no return means return NULL, which trigger this method in other plugins until reach the original overloaded method
 	}
 
-	// receives the mime64-serialized image, decode, check/create folder,
-	// store and returns the filename. False otherwise
+	// receives the mime64-serialized image, decode, check/create folder, store and returns the filename. False otherwise
 	function processInput($field, $value, $function = "") {
 		if (preg_match($this->fieldsufix, $field["field"], $regs)) {
 			if ($_GET["edit"] != "") {
-				$table = ($_GET["edit"] != "" ? $_GET["edit"] : $_GET["select"]);
+				$table = ($_GET["edit"] ? $_GET["edit"] : $_GET["select"]);
 				$fieldname = "$field[field]";
 				$filename = $fieldname . date('_ymd_His') . '.jpg';
-				$uploadDirPath = "$this->uploadPath$table";
-				$fullfilepath = "$uploadDirPath/$filename";
+				$uploadfullpath = "$this->uploadPath/$_GET[db]/$table/$field[field]";
 				$img_mime = $_POST[$fieldname];
-				if (empty($img_mime)) { return false; }
+				if (empty($img_mime)) {
+					return false;
+				}
 				$img_mime = str_replace('data:image/jpeg;base64,', '', $img_mime);
 				$img_mime = str_replace(' ', '+', $img_mime);
 				$img_blob = base64_decode($img_mime);
-				if (!file_exists($uploadDirPath)) {
-					$mkdirStatus = mkdir($uploadDirPath, 0770, true);
-					if (!$mkdirStatus) return false;
+				// create sub-directory if needed
+				if (!file_exists($uploadfullpath)) {
+					if (!mkdir($uploadfullpath, 0770, true)) {
+						return false;
+					}
 				}
-				$success = file_put_contents($fullfilepath, $img_blob);
-				if (! $success ) { return false; }
+				// create the photo file in its final location
+				if (! file_put_contents("$uploadfullpath/$filename", $img_blob) ) {
+					return false;
+				}
 			} else {
 				$filename = $value;
 			}
@@ -117,7 +126,7 @@ class AdminerCameraUpload {
 
 	function selectVal($val, &$link, $field, $original) {
 		if ($val != "" && preg_match($this->fieldsufix, $field["field"], $regs)) {
-			$link = "$this->displayPath" . $_GET['select'] . "/$val";
+			$link = "$this->displayPath/$_GET[db]/$_GET[select]/$field[field]/$val";
 		}
 	}
 
